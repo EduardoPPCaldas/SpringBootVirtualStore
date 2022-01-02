db6259739f79d4723b13713eb0f9a6e779937299
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2022-01-02 18:54:36 -0300

    itemtPedido parte 1

--- src/main/java/com/eduardo/springbootvirtualstore/domain/Pedido.java
+++ src/main/java/com/eduardo/springbootvirtualstore/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.eduardo.springbootvirtualstore.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -31,6 +33,8 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
         this.id = id;
@@ -105,5 +109,13 @@ public class Pedido implements Serializable {
         } else if (!id.equals(other.id))
             return false;
         return true;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 }
--- src/main/java/com/eduardo/springbootvirtualstore/domain/Produto.java
+++ src/main/java/com/eduardo/springbootvirtualstore/domain/Produto.java
@@ -2,7 +2,9 @@ package com.eduardo.springbootvirtualstore.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,10 +35,28 @@ public class Produto implements Serializable {
     )
     private List<Categoria> categorias = new ArrayList<>();
 
+    private Set<ItemPedido> itens = new HashSet<>();
+
     public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public List<Pedido> getPedidos(){
+        List<Pedido> lista = new ArrayList<>();
+        for (ItemPedido x : itens){
+            lista.add(x.getPedido());
+        }
+        return lista;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     public Produto() {

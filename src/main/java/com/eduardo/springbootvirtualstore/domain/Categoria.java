0928e318e7b1ee881fee9296e465d0bb8faf892c
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2022-01-03 19:56:01 -0300

    Atualização: trocando decorators para jsonignore e tirando o managereference

--- src/main/java/com/eduardo/springbootvirtualstore/domain/Categoria.java
+++ src/main/java/com/eduardo/springbootvirtualstore/domain/Categoria.java
@@ -9,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -21,7 +18,6 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
-    @JsonManagedReference
     @ManyToMany(mappedBy = "categorias")
     private List<Produto> produtos = new ArrayList<>();
     

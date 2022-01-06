526b8911a59d61b9ae106cd21a7fcf4593217de8
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2022-01-06 12:10:56 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/eduardo/springbootvirtualstore/services/CategoriaService.java
+++ src/main/java/com/eduardo/springbootvirtualstore/services/CategoriaService.java
@@ -40,8 +40,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria categoria){
-        find(categoria.getId());
-        return repo.save(categoria);
+        Categoria newObj = find(categoria.getId());
+        updateData(newObj, categoria);
+        return repo.save(newObj);
     }
 
     public void delete(Integer id){
@@ -61,5 +62,9 @@ public class CategoriaService {
     
     public Categoria fromDTO(CategoriaDTO categoriaDTO){
         return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria categoria){
+        newObj.setNome(categoria.getNome());
     }
 }

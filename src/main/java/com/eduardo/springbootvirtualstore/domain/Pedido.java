9cc5a136eea178db7f5ab37ba20b3dc3d3f7471e
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2022-01-10 20:01:20 -0300

    Subtotal e total do pedido

--- src/main/java/com/eduardo/springbootvirtualstore/domain/Pedido.java
+++ src/main/java/com/eduardo/springbootvirtualstore/domain/Pedido.java
@@ -55,6 +55,15 @@ public class Pedido implements Serializable {
     public Pedido() {
     }
 
+    public Double getValorTotal(){
+        Double soma = 0.0;
+        for (ItemPedido ip : itens) {
+            soma += ip.getSubTotal();
+        }
+
+        return soma;
+    }
+
     public Integer getId() {
         return id;
     }

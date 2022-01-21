f676362c9338a59d9cdff87a8262d748e27a7555
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2022-01-21 18:09:50 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/eduardo/springbootvirtualstore/services/ClienteService.java
+++ src/main/java/com/eduardo/springbootvirtualstore/services/ClienteService.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import java.awt.image.BufferedImage;
+
 import com.eduardo.springbootvirtualstore.domain.Cidade;
 import com.eduardo.springbootvirtualstore.domain.Cliente;
 import com.eduardo.springbootvirtualstore.domain.Endereco;
@@ -19,6 +21,7 @@ import com.eduardo.springbootvirtualstore.services.exceptions.DataIntegrityExcep
 import com.eduardo.springbootvirtualstore.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -41,6 +44,12 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id){
         UserSS user = UserService.authenticated();
@@ -128,11 +137,9 @@ public class ClienteService {
             throw new AuthorizationException("Acesso negado");
         }
 
-        URI uri = s3Service.uploadFile(multipartFile);
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + user.getId() + ".jpg";
 
-        Optional<Cliente> cli = repo.findById(user.getId());
-        cli.orElse(null).setImageUrl(uri.toString());
-
-        return uri;
+        return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
 }
